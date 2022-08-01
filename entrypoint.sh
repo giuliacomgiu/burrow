de5eee6dea26ac6655a1915d0072228bf879b34b
Giulia Ciprandi <giulia.ciprandi@yapay.com.br>
2022-07-31 23:06:53 -0300

    testing new containerized action

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,9 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /burrow_1/tmp/pids/server.pid
 
+# to do: put this on a file according to ENV var
+bundle exec rails db:setup
+bundle exec rails assets:precompile
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
